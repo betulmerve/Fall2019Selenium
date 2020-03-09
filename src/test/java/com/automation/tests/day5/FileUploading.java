6fc2ed8741240006ec0b53114da3be139fb3a546
betulmerve <merve.faki@gmail.com>
2020-03-09 17:34:46 -0400

    day5

---
+++ src/test/java/com/automation/tests/day5/FileUploading.java
@@ -0,0 +1,36 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FileUploading {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/upload");
+        BrowserUtils.wait(2);
+
+        WebElement upload=driver.findElement(By.id("file-upload"));
+        String filePath=System.getProperty("user.dir")+"/pom.xml"; //user working directory
+        upload.sendKeys(filePath);
+        BrowserUtils.wait(2);
+
+        driver.findElement(By.id("file-submit")).click(); //to upload
+        BrowserUtils.wait(2);
+        WebElement heading=driver.findElement(By.tagName("h3"));
+        System.out.println(heading.getText());
+
+        System.out.println(driver.findElement(By.id("uploaded-files")).getText());
+
+
+        driver.quit();
+    }
+}
