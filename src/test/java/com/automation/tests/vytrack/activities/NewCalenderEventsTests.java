aede875df9b9418ce8c31d4a24b5f8f03b1771ae
betulmerve <merve.faki@gmail.com>
2020-03-29 22:16:47 -0400

    day14

---
+++ src/test/java/com/automation/tests/vytrack/activities/NewCalenderEventsTests.java
@@ -0,0 +1,16 @@
+package com.automation.tests.vytrack.activities;
+
+import com.automation.tests.vytrack.AbstractTestBase;
+
+public class NewCalenderEventsTests extends AbstractTestBase {
+
+    /*
+     * Test Case: Default options
+     * Login as sales manager
+     * Go to Activities --> Calendar Events
+     * Click on Create Calendar Event
+     * Default owner name should be current user/
+
+     */
+
+}
